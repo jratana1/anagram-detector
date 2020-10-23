b549be7235130df3c72cccfdf95d831314cb2ba9
John Ratana <jratana1@gmail.com>
2020-10-22 20:02:55 -0400

    commit tree ae560c5d77b9058fa1958476f4be20f1ac79a1b1

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
 # Your code goes here!
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+
+    attr_accessor :word
+
+    def match(array)
+        array.delete_if {|word| word.split("").sort != @word.split("").sort}
+    end
+    # def match(array)
+    #     array.collect do |word| 
+    #         if word.split("").sort == "listen".split("").sort 
+    #             word
+    #         end
+    #     end
+    # end
+
+end
+
+# def match(array)
+#     array.delete_if {|word| word.split("").sort != "listen".split("").sort}
+# end
